094165b83137fb32373c4ba1f43c2c4e2d97909a
Maxdeoro <maxdeoro@gmail.com>
2023-09-09 22:50:48 +0300

    added errorMessage

---
+++ src/components/errorMessage/errorMessage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// import GotService from '../../services/gotService';
+import './errorMessage.css';
+
+const ErrorMessage = () => {
+    return <span className='err'>Somthing goes wrong !</span>
+};
+
+export default ErrorMessage;
--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import './randomChar.css';
 import GotService from '../../services/gotService';
 import Spinner from '../spinner';
+import ErrorMessage from '../errorMessage';
 
 export default class RandomChar extends Component {
     constructor() {
@@ -20,51 +21,37 @@ export default class RandomChar extends Component {
         this.setState({
             char,
             loading: false,
+            error: false,
+        });
+    };
+
+    onError = (err) => {
+        this.setState({
+            error: true,
+            loading: false,
         });
     };
 
     updateChar() {
-        const id = Math.floor(Math.random() * 120 + 25);  //chars from 25 to 140
+        const id = Math.floor(Math.random() * 1200 + 25);  //chars from 25 to 1200
         this.gotService.getCharacter(id)
-        .then(this.onCharLoaded);
+        .then(this.onCharLoaded)
+        .catch(this.onError);
     };
 
     render() {
         // const {char:{name,gender,born,died,culture,titles}, loading} = this.state;
-        const {char,loading} = this.state;
+        const {char,loading,error} = this.state;
 
-        const content = loading ? <Spinner/> : <View char={char}/>;
-
-        // if(loading) {
-        //     return <Spinner/>;
-        // }
+        const content = !(loading || error) ? <View char={char}/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const errorMessage = error ? <ErrorMessage/> : null;
 
         return (
             <div className='random-block rounded'>
                 {content}
-                {/* <h4>Random Character: {name}</h4> */}
-                {/* <ul className="list-group list-group-flush">
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className='term'>Gender</span>
-                        <span>{gender}</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className='term'>Born</span>
-                        <span>{born}</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className='term'>Died</span>
-                        <span>{died}</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className='term'>Culture</span>
-                        <span>{culture}</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className='term'>Titles</span>
-                        <span>{titles}</span>
-                    </li>
-                </ul> */}
+                {spinner}
+                {errorMessage}
             </div>
         );
     }
